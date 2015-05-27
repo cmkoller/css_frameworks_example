2f5ffdeb8ec9c90aca5093c0c94fca7abe008646
cmkoller <cmkoller01@gmail.com>
2015-05-27 16:17:01 -0400

    Add Sinatra Flash messages

--- server.rb
+++ server.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'sinatra/flash'
+
+enable :sessions
 
 get '/' do
+  erb :index
+end
+
+get '/tada' do
+  flash[:success] = "Hey! You made it!"
+  flash[:warning] = "Watch out!"
   erb :index
 end
